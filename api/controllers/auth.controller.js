72dd91096fa8523c8931e91fb01d1c3fded57cef
Yusuf ma <13893491334mys@gmail.com>
2024-01-04 02:15:00 +1000

    Complete the signUp API:Install the bcryptjs to hash the password. add respond toserver when it successfully created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,13 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const signUp = async (req, res) => {
   console.log(req.body);
   const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
+  const hashedPassword = bcryptjs.hashSync(password, 12);
+  const newUser = new User({ username, email, password: hashedPassword });
   await newUser.save();
-  res.statu;
+  res.status(201).json("User Created Successfully!");
 };
 
 export default signUp;
