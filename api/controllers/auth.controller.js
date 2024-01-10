9c18d6791b449e21cf3898cdf4aaf6891218adc7
Yusuf ma <13893491334mys@gmail.com>
2024-01-11 03:05:07 +1000

    Create signIn API route and set token as  cookie , and use object destructing and _doc, bcryptjs.compareSync

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import errorHandler from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -9,6 +11,24 @@ export const signUp = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User Created Successfully!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body; // !!! this is object not array so make it sure use {} destructing object
+  try {
+    const vaildUser = await User.findOne({ email });
+    if (!vaildUser) return next(errorHandler(404, "User Not Found!"));
+    const vaildPassword = bcryptjs.compareSync(password, vaildUser.password);
+    if (!vaildPassword) return next(errorHandler(404, "Invaild Password!"));
+    const token = jwt.sign({ id: vaildUser._id }, process.env.JWT_SECRETE); // create token (cookie) for checking user authentic.
+    const { password: pass, ...rest } = vaildUser._doc; // add _doc remove the password
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(201)
+      .json(rest);
   } catch (error) {
     next(error);
   }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,9 @@
 import express from "express";
-
-import { signUp } from "../controllers/auth.controller.js";
+import { signIn, signUp } from "../controllers/auth.controller.js";
 
 const signUpRouter = express.Router();
 
 signUpRouter.post("/signUp", signUp);
+signUpRouter.post("/signIn", signIn);
 
 export default signUpRouter;
