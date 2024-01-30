adf7f0ce5484d98c85f5740fa22a3e5c55cd1e7e
Yusuf ma <13893491334mys@gmail.com>
2024-01-30 18:10:24 +1000

    Create the uploading avatar image function

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,12 +1,84 @@
 import { useSelector } from "react-redux"
-
+import { MdEdit } from "react-icons/md";
+import { useEffect, useRef, useState } from "react";
+import{getStorage, uploadBytesResumable, ref, getDownloadURL} from 'firebase/storage'
+import { app } from "../firebase";
 export default function Profile() {
+  const fileRef = useRef(null);
   const {currentUser} = useSelector(state => state.user);
+  const [file,setFile] = useState(undefined);
+  const [filePerc,setFilePerc] = useState(0);
+  const [fileUploadError,setFileUploadError] = useState(false);
+  const [formData,setFormData] = useState({});
+  console.log(formData);
+  console.log(fileUploadError);
+//   // Craft rules based on data in your Firestore database
+// // allow write: if firestore.get(
+// //    /databases/(default)/documents/users/$(request.auth.uid)).data.isAdmin;
+// service firebase.storage {
+//   match /b/{bucket}/o {
+//     match /{allPaths=**} {
+//       allow read;
+//       allow write: if 
+//       request.resource.size < 2 * 1024 * 1024 &&
+//       request.resource.contentType.matches('image/.*')
+//     }
+//   }
+// }s
+  useEffect(() => {
+    if(file){
+      handleFileUpload(file);
+    }
+  }, [file])
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName =  new Date().getTime() + file.name;
+    const storageRef = ref (storage,fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      'state_changed', 
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred/ snapshot.totalBytes) * 100;
+        
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then
+        ((downloadURL) => 
+          setFormData({...formData, avatar: downloadURL})
+        );
+      }
+    );
+  };
+
   return (
-    <div className="p-3 max-w-lg mx-auto">
+    <div className="p-3 max-w-lg mx-auto ">
       <h1 className='text-3xl text-center font-semibold my-7'>Profile</h1>
-      <form className="flex flex-col gap-3">
-        <img src={currentUser.avatar}  alt="avatar" className="rounded-full h-24 w-24 object-cover self-center my-2 cursor-pointer"/>
+      {/* <MdEdit onClick={()=> fileRef.current.click() }  ref= {fileRef} className="cursor-pointer absolute sm: ml-72 mt-20" /> */}
+      <form className="flex flex-col gap-4 ">
+        <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept="image/*"/>
+        <img onClick={()=> fileRef.current.click() } 
+        src= {formData.avatar || currentUser.avatar}  alt="avatar" 
+        className="rounded-full h-24 w-24 object-cover self-center my-2 cursor-pointer"/>
+        <p className="text-sm text-center">
+          {fileUploadError ? (
+            <span className="text-red-700" >Error Image 
+            Upload(image size must less than 2mb)</span> 
+          ) :filePerc > 0 && filePerc < 100 ?(
+            <span className="text-slate-700">{`Uploading ${filePerc}%`
+            }</span>
+          ) : filePerc === 100 ?(
+            <span className="text-green-700">Image 
+            Successfully Uploaded</span>
+          ) :(
+            ''
+          )}
+        </p>
         <input placeholder='userName' type='text' className='border rounded-lg p-3'id="username"/>
         <input placeholder='Email' type='text' className=' border rounded-lg p-3'id="email"/>
         <input placeholder='password' type='text' className=' border rounded-lg p-3'id="passord"/>
